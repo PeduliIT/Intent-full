5a6180e1f4ee2d13572de8837078ca07164c2a94
lenovo <crewrompal@gmail.com>
2018-07-16 22:03:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/app/tes.java
@@ -0,0 +1,11 @@
+package com.example.lenovo.app;
+
+import android.content.Intent;
+
+public interface tes {
+
+    public void inputan(int aaa,int bbb,Intent data);
+        int aaa = 100;
+        int bbb = 100;
+
+}
